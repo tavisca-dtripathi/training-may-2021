3b922005fa549e447918e13a4a0299bc12b427a9
Deependra Tripathi <dtripathi@AffinionDS.Com>
2021-05-31 21:50:24 +0530

    added border color red on error

--- my-ng-app/src/app/directives/attributedirective/app.alert.directive.ts
+++ my-ng-app/src/app/directives/attributedirective/app.alert.directive.ts
@@ -1,24 +1,24 @@
-import { Directive,ElementRef, Renderer2, Input, HostListener, SimpleChanges } from "@angular/core";
+import { Directive, ElementRef, Renderer2, Input, HostListener, SimpleChanges } from "@angular/core";
 
 @Directive({
     selector: '[setAlertColor]'
 })
 export class AlertDirective {
-  constructor(private ele:ElementRef, private renderer:Renderer2){}
- 
-  @Input('setAlertColor') setAlertColor:string= '';
+    constructor(private ele: ElementRef, private renderer: Renderer2) { }
 
-  // method for logic
-    private applyColor(color:string):void {
+    @Input('setAlertColor') setAlertColor: string = '';
+
+    // method for logic
+    private applyColor(color: string): void {
         // defining a new renderingn of the HTML element by applying
         // the backgroundColor style
-        this.renderer.setStyle(this.ele.nativeElement, 'background', color );   
+        this.renderer.setStyle(this.ele.nativeElement, 'border-color', color);
     }
-   
-   ngOnChanges(changes: SimpleChanges): void {
-       if(this.setAlertColor === 'red')
-       this.applyColor(this.setAlertColor);
-       else if(this.setAlertColor === '')
-       this.applyColor('');
-   }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (this.setAlertColor === 'red')
+            this.applyColor(this.setAlertColor);
+        else if (this.setAlertColor === '')
+            this.applyColor('');
+    }
 }
